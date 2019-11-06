0fcb1624cbc68713d6e941360890681494e17c65
Evelyn Chen <evelync93@gmail.com>
2019-11-06 12:36:27 -0500

    add required server routes

--- server/index.js
+++ server/index.js
@@ -8,6 +8,23 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
-// app.get('/reviews/:product_id/list')
+app.get(`/reviews/:product_id/list`, (req, res) => {
+  let product = req.params.product_id;
+  console.log(req.query.page);
+  console.log(req.query.count);
+  console.log(req.query.sort);
+  console.log(req.query);
+  //   res.send(product);
+});
+
+app.get(`/reviews/:product_id/meta`, (req, res) => {
+  console.log(req.params.product_id);
+});
+
+app.post(`/reviews/:product_id`, (req, res) => {});
+
+app.put(`/reviews/helpful/:review_id`, (req, res) => {});
+
+app.put(`/reviews/report/:review_id`, (req, res) => {});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));

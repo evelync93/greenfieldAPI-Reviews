d46c174cce89b9889e895eb57d5db05396b0c2e2
Evelyn Chen <evelync93@gmail.com>
2019-11-06 12:05:47 -0500

    add express server

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+// app.get('/reviews/:product_id/list')
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));

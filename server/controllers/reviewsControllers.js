12d1313e8876505871d6651e933c43dd4cefc965
Evelyn Chen <evelync93@gmail.com>
2019-11-08 15:30:49 -0500

    update reviewMeta model

--- server/controllers/reviewsControllers.js
+++ server/controllers/reviewsControllers.js
@@ -19,45 +19,63 @@ module.exports = {
 
   getReviewMeta: (req, res) => {
     //TODO: need to combine output of rating and recommend
-    reviewsModel
-      .getCharacteristicsdb(req.params.product_id)
+    let productid = req.params.product_id;
+    const promises = [
+      reviewsModel.getCharacteristicsdb(productid).exec(),
+      reviewsModel.getRatingCountdb(productid).exec(),
+      reviewsModel.getRecommendCountdb(productid).exec()
+    ];
+
+    Promise.all(promises)
       .then(results => {
-        console.log(results);
-        res.send(results);
+        let characteristics = results[0];
+        let ratings = results[1];
+        let recommend = results[2];
+        let metadata = {
+          product_id: productid,
+          ratings: {},
+          recommended: {},
+          characteristics: {}
+        };
+        let charIDs = {};
+        let charDetails = {};
+        let charCount = {};
+        let charTotal = {};
+
+        for (let i = 0; i < ratings.length; i++) {
+          metadata.ratings[ratings[i]["_id"]] = ratings[i].count;
+        }
+        for (let j = 0; j < recommend.length; j++) {
+          metadata.recommended[recommend[j]["_id"]] = recommend[j].count;
+        }
+        for (let k = 0; k < characteristics.length; k++) {
+          let characteristic = characteristics[k];
+          if (charIDs[characteristic.name] === undefined) {
+            charIDs[characteristic.name] = characteristic["characteristic_id"];
+          }
+          if (charCount[characteristic.name]) {
+            charCount[characteristic.name]++;
+          } else {
+            charCount[characteristic.name] = 1;
+          }
+          if (charTotal[characteristic.name]) {
+            charTotal[characteristic.name] += characteristic.value;
+          } else {
+            charTotal[characteristic.name] = characteristic.value;
+          }
+        }
+
+        for (let key in charTotal) {
+          charDetails[key] = {};
+          charDetails[key].value = charTotal[key] / charCount[key];
+          charDetails[key].id = 1;
+        }
+        metadata.characteristics = charDetails;
+        res.send(metadata);
       })
       .catch(err => {
         console.log(err);
       });
-
-    // reviewsModel
-    //   .getRatingCountdb(req.params.product_id)
-    //   .then(results => {
-    //     res.send(results);
-    //   })
-    //   .catch(err => {
-    //     console.log(err);
-    //   });
-
-    // reviewsModel
-    //   .getRecommendCountdb(req.params.product_id)
-    //   .then(results => {
-    //     res.send(results);
-    //   })
-    //   .catch(err => {
-    //     console.log(err);
-    //   });
-
-    // reviewsModel
-    //   .getReviewMetadb(req.params.product_id)
-    //   .then(results => {
-    //     console.log(results);
-    //     res.send(results);
-    //   })
-    //   .catch(err => {
-    //     console.log(err);
-    //   });
-
-    // res.send("done");
   },
 
   postReview: (req, res) => {

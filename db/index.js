c76a48a82ede6ad2835a566c38e1851183761d35
Evelyn Chen <evelync93@gmail.com>
2019-11-11 16:09:30 -0500

    add docker changes

--- db/index.js
+++ db/index.js
@@ -1,11 +1,35 @@
 const mongoose = require("mongoose");
-if (process.env.MONGODB_URI) {
-  mongoose.connect(process.env.MONGODB_URI);
-} else {
-  mongoose.connect("mongodb://localhost:27017/greenfield", {
-    useNewUrlParser: true
-  });
-}
+
+const options = {
+  autoIndex: false, // Don't build indexes
+  reconnectTries: 30, // Retry up to 30 times
+  reconnectInterval: 500, // Reconnect every 500ms
+  poolSize: 10, // Maintain up to 10 socket connections
+  // If not connected, return errors immediately rather than waiting for reconnect
+  bufferMaxEntries: 0
+};
+
+// if (process.env.MONGODB_URI) {
+//   mongoose.connect(process.env.MONGODB_URI);
+// } else {
+//   mongoose.connect("mongodb://mongo:27017/greenfield", {
+//     useNewUrlParser: true
+//   });
+// }
+
+const connectWithRetry = () => {
+  console.log("MongoDB connection with retry");
+  mongoose
+    .connect("mongodb://mongo:27017/test", options)
+    .then(() => {
+      console.log("MongoDB is connected");
+    })
+    .catch(err => {
+      console.log("MongoDB connection unsuccessful, retry after 5 seconds.");
+      setTimeout(connectWithRetry, 5000);
+    });
+};
+connectWithRetry();
 
 const db = mongoose.connection;
 

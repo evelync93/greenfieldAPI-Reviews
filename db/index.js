ced82796291d281628f5ac724057aeac286a99e6
Evelyn Chen <evelync93@gmail.com>
2019-11-13 19:36:42 -0500

    update mongo connect link

--- db/index.js
+++ db/index.js
@@ -12,9 +12,12 @@ const mongoose = require("mongoose");
 if (process.env.MONGODB_URI) {
   mongoose.connect(process.env.MONGODB_URI);
 } else {
-  mongoose.connect("mongodb://localhost:27017/greenfield", {
-    useNewUrlParser: true
-  });
+  mongoose.connect(
+    "mongodb://ec2-3-133-94-163.us-east-2.compute.amazonaws.com:27017/greenfield",
+    {
+      useNewUrlParser: true
+    }
+  );
 }
 
 // const connectWithRetry = () => {

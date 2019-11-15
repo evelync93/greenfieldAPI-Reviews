b45cedc03bf24af5428b5ac95ffe4f6ac20dfb9e
Evelyn Chen <evelync93@gmail.com>
2019-11-15 02:15:12 -0500

    remove unused code

--- db/index.js
+++ db/index.js
@@ -1,14 +1,5 @@
 const mongoose = require("mongoose");
 
-// const options = {
-//   autoIndex: false, // Don't build indexes
-//   reconnectTries: 30, // Retry up to 30 times
-//   reconnectInterval: 500, // Reconnect every 500ms
-//   poolSize: 10, // Maintain up to 10 socket connections
-//   // If not connected, return errors immediately rather than waiting for reconnect
-//   bufferMaxEntries: 0
-// };
-
 if (process.env.MONGODB_URI) {
   mongoose.connect(process.env.MONGODB_URI);
 } else {
@@ -16,20 +7,6 @@ if (process.env.MONGODB_URI) {
     useNewUrlParser: true
   });
 }
-
-// const connectWithRetry = () => {
-//   console.log("MongoDB connection with retry");
-//   mongoose
-//     .connect("mongodb://mongo:27017/test", options)
-//     .then(() => {
-//       console.log("MongoDB is connected");
-//     })
-//     .catch(err => {
-//       console.log("MongoDB connection unsuccessful, retry after 5 seconds.");
-//       setTimeout(connectWithRetry, 5000);
-//     });
-// };
-// connectWithRetry();
 
 const db = mongoose.connection;
 

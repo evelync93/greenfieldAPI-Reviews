6f71a540063c9b6af25640229d498b035c11d23e
Evelyn Chen <evelync93@gmail.com>
2019-11-18 19:16:29 -0500

    remove comments

--- db/index.js
+++ db/index.js
@@ -14,7 +14,6 @@ mongoose.Promise = Promise;
 db.on("error", console.error.bind(console, "Connection error:"));
 db.once("open", () => {
   console.log("Connected to db...");
-  // getReviewID()
 });
 
 module.exports.db = db;
